1b00903ca8c3192b3adc00da3439ca99594bdde0
Yash Sharma <yasharmaster@gmail.com>
2018-03-17 22:08:37 +0530

    Speech recognition & storing & retriving working perfectly

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -40,8 +40,8 @@ window.App = {
       accounts = accs;
       account = accounts[0];
       console.log(account);
-
-        self.refreshEntries();
+      
+      self.refreshEntries();
     });
 
   },
@@ -67,6 +67,7 @@ window.App = {
     }).then(function() {
       self.setStatus("Diary entry added!");
       self.refreshEntries();
+      location.reload();
     }).catch(function(e) {
       console.log(e);
       self.setStatus("Error sending coin; see log.");
@@ -82,6 +83,7 @@ window.App = {
       return meta.getEntries.call({from: account});
     }).then(function(value) {
       // var entries_element = document.getElementById("all-entries");
+      console.log("Retrieved values are : " + value);
       var str_array = value.split(',');
       var ul = document.getElementById('all-entries');
       var li;
@@ -150,4 +152,111 @@ window.addEventListener('load', function() {
   }
 
   App.start();
+
+try {
+  console.log("in speech");
+  var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  var recognition = new SpeechRecognition();
+}
+catch(e) {
+  console.error(e);
+  alert("Please upgrade your browser");
+}
+
+var noteTextarea = $('#new-content');
+var instructions = $('#recording-instructions');
+
+var noteContent = '';
+
+/*-----------------------------
+      Voice Recognition 
+------------------------------*/
+
+// If false, the recording will stop after a few seconds of silence.
+// When true, the silence period is longer (about 15 seconds),
+// allowing us to keep recording even when the user pauses. 
+recognition.continuous = true;
+
+// This block is called every time the Speech APi captures a line. 
+recognition.onresult = function(event) {
+
+  // event is a SpeechRecognitionEvent object.
+  // It holds all the lines we have captured so far. 
+  // We only need the current one.
+  var current = event.resultIndex;
+
+  // Get a transcript of what was said.
+  var transcript = event.results[current][0].transcript;
+
+  // Add the current transcript to the contents of our Note.
+  // There is a weird bug on mobile, where everything is repeated twice.
+  // There is no official solution so far so we have to handle an edge case.
+  var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
+
+  if(!mobileRepeatBug) {
+    noteContent += transcript;
+    noteTextarea.val(noteContent);
+  }
+};
+
+recognition.onstart = function() { 
+  instructions.text('Voice recognition activated. Try speaking into the microphone.');
+}
+
+recognition.onspeechend = function() {
+  instructions.text('You were quiet for a while so voice recognition turned itself off.');
+}
+
+recognition.onerror = function(event) {
+  if(event.error == 'no-speech') {
+    instructions.text('No speech was detected. Try again.');  
+  };
+}
+
+
+
+/*-----------------------------
+      App buttons and input 
+------------------------------*/
+
+$('#start-record-btn').on('click', function(e) {
+  if (noteContent.length) {
+    noteContent += ' ';
+  }
+  recognition.start();
+});
+
+
+$('#pause-record-btn').on('click', function(e) {
+  recognition.stop();
+  instructions.text('Voice recognition paused.');
+});
+
+// Sync the text inside the text area with the noteContent variable.
+noteTextarea.on('input', function() {
+  noteContent = $(this).val();
+})
+
+
+/*-----------------------------
+      Speech Synthesis 
+------------------------------*/
+
+function readOutLoud(message) {
+  var speech = new SpeechSynthesisUtterance();
+
+  // Set the text and voice attributes.
+  speech.text = message;
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
+  
+  window.speechSynthesis.speak(speech);
+}
+
+
+
+
+
+
 });
